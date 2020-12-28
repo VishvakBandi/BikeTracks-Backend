9ca95595de58a5b12f091ce40e3fab6ef17d38c2
vishvakbandi <vishvakbandi@gmail.com>
2020-12-28 17:48:50 -0600

    remove files

--- config.js
+++
@@ -1,7 +0,0 @@
-// config, contains API keys
-
-var config = {
-    mongoUri : "mongodb+srv://vishvakbandi:cjrF1DYixKJVhfgp@cluster0.q5jud.mongodb.net/<dbname>?retryWrites=true&w=majority"
-}
-
-export { config }
